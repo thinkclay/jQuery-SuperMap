522a433a141817b6bd3db013f01986295e3775f2
Clayton McIlrath <dev@bychosen.com>
2011-08-01 22:14:36 -0600

    Stable: Slide, button click, hovers, image scale on zoom Dev:	Zoom, Coordinate Movement, Point Resize Bugs:	On zoom, re-set boundries

--- js/init.js
+++ js/init.js
@@ -1,18 +1,47 @@
-$(function(){
+function map_instance ()
+{
 	$('.map').supermap({
-		position: 'center',
-		popupClass: 'bubble',
-		markerClass: 'point',
-		popup: true,
-		cookies: true,
-		caption: true,
-		setCenter: true,
-		navigation: true,
-		navSpeed: 1000,
-		navBtnClass: 'navBtn',
+		position: 		'center',
+		popupClass: 	'slide',	// I created styles for 'bubble' and 'slide' here
+		markerClass: 	'point',
+		popup: 			'slide',
+		cookies: 		true,
+		caption: 		false,
+		setCenter: 		true,
+		navigation: 	true,
+		navSpeed: 		1000,
+		navBtnClass: 	'map-move',
 		outsideButtons: '.map_buttons a',
-		onMarkerClick: function(){},
-		onPopupClose: function(){},
-		onMapLoad: function(){}
+		onMarkerClick: 	function(){},
+		onPopupClose: 	function(){},
+		onMapLoad: 		function(){}
 	});
+}
+
+$(function(){
+	map_instance();
+
+	$('#map_zoom #zoom_in').click( function () {
+		map_zoom('in');
+		$('.map').supermap_refresh();
+		return false;
+	});
+	
+	$('#map_zoom #zoom_out').click( function () {
+		map_zoom('out');
+		$('.map').supermap_refresh();
+		return false;
+	});
+/*	
+	$('.activity').hover(
+		function () {
+			if ( ! $(this).hasClass('selected') ) 
+				$(this).animate({ 'opacity': .4 }); 
+		},
+		function () { 
+			if ( ! $(this).hasClass('selected') )
+				$(this).animate({ 'opacity': 0 }); 
+		}
+	);
+*/
 });
--- js/supermap.js
+++ js/supermap.js
@@ -7,33 +7,38 @@
 (function ($) {
     $.fn.supermap = function (options) {
         var defaults = {
-            position: "center",
-            popupClass: "bubble",
-            markerClass: "point",
-            popup: true,
-            cookies: true,
-            caption: true,
-            setCenter: true,
-            navigation: true,
-            navSpeed: 1000,
-            navBtnClass: "navBtn",
+            position: 		'center',
+            popupClass: 	'bubble',
+            markerClass: 	'point',
+            popup: 			'bubble',		// slide or bubble
+            cookies: 		true,
+            caption: 		true,
+            setCenter: 		true,
+            navigation: 	true,
+            navSpeed: 		1000,
+            navBtnClass: 	'map-move',
             outsideButtons: false,
-            onMarkerClick: function () {},
-            onPopupClose: function () {},
-            onMapLoad: function () {}
+            onMarkerClick: 	function () {},
+            onPopupClose: 	function () {},
+            onMapLoad: 		function () {}
         };
+        
         var sets = $.extend({}, defaults, options);
-        return this.each(function () {
+        
+        return this.each( function () 
+        {
             var $this = $(this);
             $this.css({
                 position: "relative",
                 overflow: "hidden",
                 cursor: "move"
             });
+            
             $this.wrapInner($("<div />").addClass("imgContent").css({
                 zIndex: "1",
                 position: "absolute"
             }));
+            
             var content = $this.find(".imgContent"),
                 image = $this.find("img"),
                 title = image.attr("alt"),
@@ -43,6 +48,7 @@
                 imgh = image.height(),
                 divw = $this.width(),
                 divh = $this.height();
+                
             var cookies = {
                 create: function (name, value, days) {
                     if (days) {
@@ -276,6 +282,7 @@
                     }
                 }
             };
+            
             if (sets.navigation) {
                 map.navigation.buttons();
                 map.navigation.move()
@@ -326,19 +333,28 @@
                 }
             });
             map.init(sets.position);
+            
             point.each(function () {
                 var $this = $(this),
                     pos = $this.attr("rel").split("-");
-                x = pos[1], y = pos[2];
+                	x = pos[1], y = pos[2];
+                
                 $this.css({
                     position: "absolute",
                     zIndex: "2",
                     top: y + "px",
                     left: x + "px"
                 })
-            }).wrapInner($("<div />").addClass("markerContent").css({
-                display: "none"
-            }));
+                
+                $this.wrapInner($("<div />").addClass("markerContent").css({ display: "none" }));
+                
+                if ($this.attr('data-type')	== 'image') {
+                	prepend = '<img src="/img/map/'+$this.attr('data-prefix')+$this.attr('id')+'.png" alt="'+$this.attr('id')+'" />';
+                	$this.prepend(prepend);
+                }
+            });
+            
+            
             point.click(function () {
                 var $this = $(this),
                     pointw = $this.width(),
@@ -347,6 +363,10 @@
                     py = pos.top,
                     px = pos.left,
                     wrap = $this.find(".markerContent").html();
+                    
+                $('.point').removeClass('selected');
+                $this.addClass('selected');
+                
                 if (sets.setCenter) {
                     var center_y = -py + divh / 2 - pointh / 2,
                         center_x = -px + divw / 2 - pointw / 2,
@@ -356,7 +376,9 @@
                         left: center.x + "px"
                     })
                 }
-                if (sets.popup) {
+                
+                if ( sets.popup == 'bubble' ) 
+                {
                     $("." + sets.popupClass).remove();
                     $this.after($("<div />").addClass(sets.popupClass).css({
                         position: "absolute",
@@ -372,13 +394,24 @@
                         left: x + "px",
                         marginLeft: -(popupw / 2 - pointw / 2) + "px"
                     })
-                } else {
+                } 
+                else if ( sets.popup == 'slide' )
+                {
+                	$("."+sets.popupClass).remove();
+                	$slide = $("<div />").addClass(sets.popupClass).html(wrap).append($('<a />').addClass('close'));
+                	$('.map').prepend($slide);
+                	$slide.slideDown();
+                }
+                else 
+                {
                     sets.onMarkerClick.call(this)
                 }
                 return false
             });
             $this.find(".close").live("click", function () {
                 var $this = $(this);
+                $('.point').removeClass('selected');
+                
                 $this.parents("." + sets.popupClass).remove();
                 setTimeout(function () {
                     sets.onPopupClose.call(this)
@@ -393,9 +426,323 @@
                         return $(this).attr("id") == id.substring(1);
                     });
                     div.click();
+                    div.addClass('selected');
+                    
                     return false
                 })
             }
         })
     }
 }(jQuery));
+
+
+
+
+
+
+
+
+
+(function ($) {
+    $.fn.supermap_refresh = function (options) {
+        
+        var sets = $.extend({}, defaults, options);
+        
+        return this.each( function () 
+        {
+            var $this = $(this);
+            
+            var content = $this.find(".imgContent"),
+                image = $this.find("img"),
+                title = image.attr("alt"),
+                point = $this.find("." + sets.markerClass),
+                mouseDown = false,
+                mx, my, ex, ey, imgw = image.width(),
+                imgh = image.height(),
+                divw = $this.width(),
+                divh = $this.height();
+                
+            var map = {
+                check: function (x, y) {
+                    if (y < (divh - imgh)) {
+                        y = divh - imgh
+                    } else {
+                        if (y > 0) {
+                            y = 0
+                        }
+                    }
+                    if (x < (divw - imgw)) {
+                        x = divw - imgw
+                    } else {
+                        if (x > 0) {
+                            x = 0
+                        }
+                    }
+                    return {
+                        x: x,
+                        y: y
+                    }
+                },
+                init: function (position) {
+                    switch (position) {
+                    case "center":
+                        var x = (divw - imgw) / 2,
+                            y = (divh - imgh) / 2;
+                        break;
+                    case "top left":
+                        var x = 0,
+                            y = 0;
+                        break;
+                    case "top right":
+                        var x = divw - imgw,
+                            y = 0;
+                        break;
+                    case "bottom left":
+                        var x = 0,
+                            y = divh - imgh;
+                        break;
+                    case "bottom right":
+                        var x = divw - imgw,
+                            y = divh - imgh;
+                        break;
+                    default:
+                        var new_position = position.split(" "),
+                            x = -(new_position[0]),
+                            y = -(new_position[1]);
+                        if (y < (divh - imgh)) {
+                            y = divh - imgh
+                        } else {
+                            if (y > 0) {
+                                y = 0
+                            }
+                        }
+                        if (x < (divw - imgw)) {
+                            x = divw - imgw
+                        } else {
+                            if (x > 0) {
+                                x = 0
+                            }
+                        }
+                    }
+                    if (sets.cookies) {
+                        if (cookies.read("position") != null) {
+                            var pos = cookies.read("position").split(","),
+                                x = pos[0],
+                                y = pos[1]
+                        } else {
+                            var x = (divw - imgw) / 2,
+                                y = (divh - imgh) / 2
+                        }
+                    }
+                    content.css({
+                        top: y + "px",
+                        left: x + "px"
+                    })
+                },
+                mouse: function (e) {
+                    var x = e.pageX,
+                        y = e.pageY;
+                    return {
+                        x: x,
+                        y: y
+                    }
+                },
+                update: function (e) {
+                    var mouse = map.mouse(e),
+                        x = mouse.x,
+                        y = mouse.y,
+                        movex = x - mx,
+                        movey = y - my,
+                        top = ey + movey,
+                        left = ex + movex,
+                        check = map.check(left, top);
+                    content.css({
+                        top: check.y + "px",
+                        left: check.x + "px"
+                    });
+                    if (sets.cookies) {
+                        cookies.create("position", check.x + "," + check.y, 7)
+                    }
+                },
+            };
+            
+            content.bind({
+                mousedown: function (e) {
+                    e.preventDefault();
+                    mouseDown = true;
+                    var mouse = map.mouse(e);
+                    mx = mouse.x, my = mouse.y;
+                    var element = content.position();
+                    ex = element.left, ey = element.top;
+                    map.update(e)
+                },
+                mousemove: function (e) {
+                    if (mouseDown) {
+                        map.update(e)
+                    }
+                    return false
+                },
+                mouseup: function () {
+                    if (mouseDown) {
+                        mouseDown = false
+                    }
+                    return false
+                },
+                mouseout: function () {
+                    if (mouseDown) {
+                        mouseDown = false
+                    }
+                    return false
+                }
+            });
+            map.init(sets.position);
+            
+            point.each(function () {
+                var $this = $(this),
+                    pos = $this.attr("rel").split("-");
+                	x = pos[1], y = pos[2];
+                
+                $this.css({
+                    position: "absolute",
+                    zIndex: "2",
+                    top: y + "px",
+                    left: x + "px"
+                })
+                
+                $this.wrapInner($("<div />").addClass("markerContent").css({ display: "none" }));
+                
+                if ($this.attr('data-type')	== 'image') {
+                	prepend = '<img src="/img/map/'+$this.attr('data-prefix')+$this.attr('id')+'.png" alt="'+$this.attr('id')+'" />';
+                	$this.prepend(prepend);
+                }
+            });
+            
+            point.click(function () {
+                var $this = $(this),
+                    pointw = $this.width(),
+                    pointh = $this.height(),
+                    pos = $this.position(),
+                    py = pos.top,
+                    px = pos.left,
+                    wrap = $this.find(".markerContent").html();
+                    
+                $('.point').removeClass('selected');
+                $this.addClass('selected');
+                
+                if (sets.setCenter) {
+                    var center_y = -py + divh / 2 - pointh / 2,
+                        center_x = -px + divw / 2 - pointw / 2,
+                        center = map.check(center_x, center_y);
+                    content.animate({
+                        top: center.y + "px",
+                        left: center.x + "px"
+                    })
+                }
+                
+                if ( sets.popup == 'bubble' ) 
+                {
+                    $("." + sets.popupClass).remove();
+                    $this.after($("<div />").addClass(sets.popupClass).css({
+                        position: "absolute",
+                        zIndex: "3"
+                    }).html(wrap).append($("<a />").addClass("close")));
+                    var popup = $this.next("." + sets.popupClass),
+                        popupw = popup.innerWidth(),
+                        popuph = popup.innerHeight(),
+                        y = py,
+                        x = px;
+                    popup.css({
+                        top: y + pointh + "px",
+                        left: x + "px",
+                        marginLeft: -(popupw / 2 - pointw / 2) + "px"
+                    })
+                } 
+                else if ( sets.popup == 'slide' )
+                {
+                	$("."+sets.popupClass).remove();
+                	$slide = $("<div />").addClass(sets.popupClass).html(wrap).append($('<a />').addClass('close'));
+                	$('.map').prepend($slide);
+                	$slide.slideDown();
+                }
+                else 
+                {
+                    sets.onMarkerClick.call(this)
+                }
+                return false
+            });
+            $this.find(".close").live("click", function () {
+                var $this = $(this);
+                $('.point').removeClass('selected');
+                
+                $this.parents("." + sets.popupClass).remove();
+                setTimeout(function () {
+                    sets.onPopupClose.call(this)
+                }, 100);
+                return false
+            });
+            if (sets.outsideButtons) {
+                $(sets.outsideButtons).click(function () {
+                    var $this = $(this),
+                        id = $this.attr("href");
+                    div = content.find("." + sets.markerClass).filter(function () {
+                        return $(this).attr("id") == id.substring(1);
+                    });
+                    div.click();
+                    div.addClass('selected');
+                    
+                    return false
+                })
+            }
+        })
+    }
+}(jQuery));
+
+function map_zoom ( direction )	
+{
+	map_width = $('#map-bg').width();
+	
+	map_vars();
+		
+	if ( direction == 'in' ) {
+		map_vars();
+		$('#map-bg').width( (map_width += 100) );		
+		$('#map-bg').height( map_width * y_ratio );
+		
+		$('.map-point').each( function () {
+			console.log($(this).position());
+			console.log( 'pointer: ' + $(this).attr('href') + ' ' + map_get_quadrant($(this).position().left, $(this).position().top) );
+			
+			$(this).css({ 'top': $(this).position().top + y_step, 'left': $(this).position().left + x_step });
+		});
+	}
+	
+	if ( direction == 'out' ) {
+		map_vars();
+		$('#map-bg').width( (map_width -= 100) );
+		$('#map-bg').height( map_width * y_ratio );
+		
+		$('.map-region img').each( function () {
+			width = $(this).width();
+			$(this).width( width / x_ratio );
+		});
+		
+		$('.map-point').each( function () {			
+			$(this).css({ 'top': $(this).position().top - 48, 'left': $(this).position().left + 29 });
+		});
+	}
+	
+	x_offset = parseInt( - ( $('#map-bg').width() - $('#map-bg').width() ) / 2 );	
+	y_offset = parseInt( - ( $('#map-bg').height() - $('#map-bg').height() ) / 2 );
+		
+	$('#map-bg').css('top', y_offset);
+	$('#map-bg').css('left', x_offset);	
+}
+
+
+function map_vars ()
+{
+	x_ratio = $('#map-bg').width() / $('#map-bg').height(); // 1.666
+	y_ratio = $('#map-bg').height() / $('#map-bg').width(); // .6
+	x_step	= x_ratio * 100;
+	y_step	= y_ratio * 100;	
+}
